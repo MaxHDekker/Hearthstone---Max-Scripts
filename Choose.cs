6c3a7efdac7f6b92271a0de4f5de5b147203ae51
Max Dekker <xamrekked@gmail.com>
2018-02-07 10:59:31 +0100

    Max all scripts
    All max zijn scripts

---
+++ Choose.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Choose : MonoBehaviour
+     , IPointerClickHandler
+{
+    public bool keep = true;
+
+    public bool crossOn = false;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        keep = !keep;
+    }
+
+    void Update()
+    {
+        if (!keep)
+        {
+            this.gameObject.transform.GetChild(7).gameObject.SetActive(true);
+        }
+        else if(keep)
+        {
+            this.gameObject.transform.GetChild(7).gameObject.SetActive(false);
+        }
+
+    }
+}
---
+++ endTurn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endTurn : MonoBehaviour
+{
+
+    public bool turnover = false;
+    public GameObject begingame;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "endturn")
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    turnover = true;
+                        Destroy(begingame);
+                }
+            }
+        }
+    }
+}
